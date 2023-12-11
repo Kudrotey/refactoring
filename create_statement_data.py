41e21bbde9959d130fcab35253f662ae3de7912b
Kudrotey <kudrotey.khuda@madetech.com>
2023-12-11 07:36:31 +0000

    Refactor: Moves amountFor into the PerformanceCalculator class.

--- create_statement_data.py
+++ create_statement_data.py
@@ -1,5 +1,26 @@
 import math
 
+class PerformanceCalculator():
+    def __init__(self, aPerformance, aPlay):
+        self.aPerformance = aPerformance
+        self.aPlay = aPlay
+
+    def amount(self):
+        result = 0
+        match self.aPlay.get('type'):
+            case "tragedy":
+                result = 40000
+                if self.aPerformance.get('audience') > 30:
+                    result += 1000 * (self.aPerformance.get('audience') - 30)
+            case "comedy":
+                result = 40000
+                if self.aPerformance.get('audience') > 20:
+                    result += 500 * (self.aPerformance.get('audience') - 20)
+                result += 300 * self.aPerformance.get('audience')
+            case _:
+                raise Exception(f"unknown type: {self.aPlay.get('type')}")
+        return result
+
 class Statement():
     def __init__(self, invoice, plays):
         self.invoice = invoice
@@ -14,9 +35,10 @@ class Statement():
         return statementData
     
     def enrichPerformance(self, aPerformance):
+        calculator = PerformanceCalculator(aPerformance, self.playFor(aPerformance))
         result = aPerformance
-        result['play'] = self.playFor(result)
-        result['amount'] = self.amountFor(result)
+        result['play'] = calculator.aPlay
+        result['amount'] = calculator.amount()
         result['volumeCredits'] = self.volumeCreditsFor(result)
         return result
     
@@ -24,20 +46,7 @@ class Statement():
         return self.plays[aPerformance.get("playID")]
     
     def amountFor(self, aPerformance):
-        result = 0
-        match aPerformance['play'].get('type'):
-            case "tragedy":
-                result = 40000
-                if aPerformance.get('audience') > 30:
-                    result += 1000 * (aPerformance.get('audience') - 30)
-            case "comedy":
-                result = 40000
-                if aPerformance.get('audience') > 20:
-                    result += 500 * (aPerformance.get('audience') - 20)
-                result += 300 * aPerformance.get('audience')
-            case _:
-                raise Exception(f"unknown type: {aPerformance['play'].get('type')}")
-        return result
+        return PerformanceCalculator(aPerformance, self.playFor(aPerformance)).amount()
     
     def volumeCreditsFor(self, aPerformance):
         result = 0
